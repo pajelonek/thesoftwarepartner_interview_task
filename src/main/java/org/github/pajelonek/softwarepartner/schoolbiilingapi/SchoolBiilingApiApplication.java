21b6268334e59119b721aab1fe92435d9db7e06e
Pawel Jelonek <pawel.jelonek.krakow@gmail.com>
2024-02-16 20:09:08 +0100

    Adding project template from spring initializer

---
+++ src/main/java/org/github/pajelonek/softwarepartner/schoolbiilingapi/SchoolBiilingApiApplication.java
@@ -0,0 +1,13 @@
+package org.github.pajelonek.softwarepartner.schoolbiilingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBiilingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBiilingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/github/pajelonek/softwarepartner/schoolbiilingapi/SchoolBiilingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.pajelonek.softwarepartner.schoolbiilingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBiilingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
